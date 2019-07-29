8ba8cc8e36b9fb332cf369b31070e67184dbd666
Borges-Porteous <miguel.borges-porteous@tbs-sct.gc.ca>
2019-07-29 10:56:24 -0400

    fixed validation screen focus on error issue

--- post-validation/revenue-agency/services/calculator/files/benefits-estimator.js
+++ post-validation/revenue-agency/services/calculator/files/benefits-estimator.js
@@ -1,17 +1,19 @@
 function validateElement(idLegend, idError, strErrorMsg, top) {
   elementTop = getOffsetTop(document.getElementById(idLegend))
+  console.log(elementTop);
   if (document.getElementById(idError) !=null) {
     $("#"+idError).removeClass('hidden');
   } else {
     $("#"+idLegend).append('<strong style="margin-top:10px;" class="error" id="'+idError+'"><span class="label label-danger" style="text-align: left; display:block;">'+strErrorMsg+'</span></strong>');
   }
-  if (top > elementTop) {
-    top = elementTop;
+  if (top[0] > elementTop) {
+    top[0] = elementTop;
+    top[1] = idLegend;
   }
   return top;
 }
 
-//Get top based on page height instead of parent element
+//Get top based on all parent elements, instead of just parent element
 const getOffsetTop = element => {
   let offsetTop = 0;
   while(element) {
